1915182dc49d0adac24a437382738e68d3de8408
liwenbo <liwb@hundsun.com>
2015-12-31 10:35:58 +0800

    add function

--- Script/src/zero-intro.js
+++ Script/src/zero-intro.js
@@ -15,6 +15,22 @@ zero.params = {
     modalCloseByOutside: false,
     actionsCloseByOutside: true,
     popupCloseByOutside: true,
-    modalStack: true,
-
+    modalStack: true
 }
+
+/**
+     * Check whether the object has the property.
+     *
+     * @param {Object} obj
+     * @param {String} key
+     * @return {Boolean}
+     */
+
+    function hasOwn(obj, key) {
+        return hasOwnProperty.call(obj, key);
+    }
+
+    function isArray(arr) {
+        if (Object.prototype.toString.apply(arr) === '[object Array]') return true;
+        else return false;
+    }
