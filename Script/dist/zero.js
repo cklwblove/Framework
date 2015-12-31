170711d2c649f61a42ec81eed33510cac7630099
liwenbo <liwb@hundsun.com>
2015-12-31 10:38:46 +0800

    build zero

--- Script/dist/zero.js
+++ Script/dist/zero.js
@@ -20,9 +20,25 @@
         modalCloseByOutside: false,
         actionsCloseByOutside: true,
         popupCloseByOutside: true,
-        modalStack: true,
-    
+        modalStack: true
     }
+    
+    /**
+         * Check whether the object has the property.
+         *
+         * @param {Object} obj
+         * @param {String} key
+         * @return {Boolean}
+         */
+    
+        function hasOwn(obj, key) {
+            return hasOwnProperty.call(obj, key);
+        }
+    
+        function isArray(arr) {
+            if (Object.prototype.toString.apply(arr) === '[object Array]') return true;
+            else return false;
+        }
 
     /*======================================================
      ************   Dom   ************
