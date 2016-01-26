b63cad09a1f0961479f00a125c81070723104f1a
lwbgithub <lwbhtml@163.com>
2016-01-26 21:01:44 +0800

    Update util.js

--- Script/src/util.js
+++ Script/src/util.js
@@ -61,6 +61,39 @@ function formatDate() {
     }
     return pattern;
 }
+//时间戳转换为几个月前，几周前，几天前，几分钟前
+function getDateDiff(dateTimeStamp){
+	var minute = 1000 * 60;
+	var hour = minute * 60;
+	var day = hour * 24;
+	var halfamonth = day * 15;
+	var month = day * 30;
+	var now = new Date().getTime();
+	var diffValue = now - dateTimeStamp;
+	if(diffValue < 0){return;}
+	var monthC =diffValue/month;
+	var weekC =diffValue/(7*day);
+	var dayC =diffValue/day;
+	var hourC =diffValue/hour;
+	var minC =diffValue/minute;
+	if(monthC>=1){
+		result="" + parseInt(monthC) + "月前";
+	}
+	else if(weekC>=1){
+		result="" + parseInt(weekC) + "周前";
+	}
+	else if(dayC>=1){
+		result=""+ parseInt(dayC) +"天前";
+	}
+	else if(hourC>=1){
+		result=""+ parseInt(hourC) +"小时前";
+	}
+	else if(minC>=1){
+		result=""+ parseInt(minC) +"分钟前";
+	}else
+	result="刚刚";
+	return result;
+}
 
 /**
  * 计算字符的长度，中文两个，英文一个，特殊字符除外
